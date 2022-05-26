e7dd7ffaf9ee885dc159e3ef89368902bb1b749e
Manel.Defnoune <36042435+manel009@users.noreply.github.com>
2022-05-26 15:10:56 +0200

    select j query en composant react

--- hr-net/src/components/home/Home.js
+++ hr-net/src/components/home/Home.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React from 'react';
 import { Link } from "react-router-dom";
 import{connect} from "react-redux";
+import {Select} from '../select/Select.js'
 
 //to get from store
 const mapStateToProps = state => {
@@ -22,7 +23,6 @@ function Home(props){
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("handleSubmit");
     let newEmployee = {
         firstName: event.target.firstname.value,
         lastName: event.target.lastname.value,
@@ -34,9 +34,268 @@ function Home(props){
         state: event.target.state.value,
         zipCode: event.target.zipcode.value
     };
-    console.log(newEmployee);
     props.saveEmployee(newEmployee);
   }
+
+  const statesContent = [
+    {
+        "text": "Alabama",
+        "value": "AL"
+    },
+    {
+        "text": "Alaska",
+        "value": "AK"
+    },
+    {
+        "text": "American Samoa",
+        "value": "AS"
+    },
+    {
+        "text": "Arizona",
+        "value": "AZ"
+    },
+    {
+        "text": "Arkansas",
+        "value": "AR"
+    },
+    {
+        "text": "California",
+        "value": "CA"
+    },
+    {
+        "text": "Colorado",
+        "value": "CO"
+    },
+    {
+        "text": "Connecticut",
+        "value": "CT"
+    },
+    {
+        "text": "Delaware",
+        "value": "DE"
+    },
+    {
+        "text": "District Of Columbia",
+        "value": "DC"
+    },
+    {
+        "text": "Federated States Of Micronesia",
+        "value": "FM"
+    },
+    {
+        "text": "Florida",
+        "value": "FL"
+    },
+    {
+        "text": "Georgia",
+        "value": "GA"
+    },
+    {
+        "text": "Guam",
+        "value": "GU"
+    },
+    {
+        "text": "Hawaii",
+        "value": "HI"
+    },
+    {
+        "text": "Idaho",
+        "value": "ID"
+    },
+    {
+        "text": "Illinois",
+        "value": "IL"
+    },
+    {
+        "text": "Indiana",
+        "value": "IN"
+    },
+    {
+        "text": "Iowa",
+        "value": "IA"
+    },
+    {
+        "text": "Kansas",
+        "value": "KS"
+    },
+    {
+        "text": "Kentucky",
+        "value": "KY"
+    },
+    {
+        "text": "Louisiana",
+        "value": "LA"
+    },
+    {
+        "text": "Maine",
+        "value": "ME"
+    },
+    {
+        "text": "Marshall Islands",
+        "value": "MH"
+    },
+    {
+        "text": "Maryland",
+        "value": "MD"
+    },
+    {
+        "text": "Massachusetts",
+        "value": "MA"
+    },
+    {
+        "text": "Michigan",
+        "value": "MI"
+    },
+    {
+        "text": "Minnesota",
+        "value": "MN"
+    },
+    {
+        "text": "Mississippi",
+        "value": "MS"
+    },
+    {
+        "text": "Missouri",
+        "value": "MO"
+    },
+    {
+        "text": "Montana",
+        "value": "MT"
+    },
+    {
+        "text": "Nebraska",
+        "value": "NE"
+    },
+    {
+        "text": "Nevada",
+        "value": "NV"
+    },
+    {
+        "text": "New Hampshire",
+        "value": "NH"
+    },
+    {
+        "text": "New Jersey",
+        "value": "NJ"
+    },
+    {
+        "text": "New Mexico",
+        "value": "NM"
+    },
+    {
+        "text": "New York",
+        "value": "NY"
+    },
+    {
+        "text": "North Carolina",
+        "value": "NC"
+    },
+    {
+        "text": "North Dakota",
+        "value": "ND"
+    },
+    {
+        "text": "Northern Mariana Islands",
+        "value": "MP"
+    },
+    {
+        "text": "Ohio",
+        "value": "OH"
+    },
+    {
+        "text": "Oklahoma",
+        "value": "OK"
+    },
+    {
+        "text": "Oregon",
+        "value": "OR"
+    },
+    {
+        "text": "Palau",
+        "value": "PW"
+    },
+    {
+        "text": "Pennsylvania",
+        "value": "PA"
+    },
+    {
+        "text": "Puerto Rico",
+        "value": "PR"
+    },
+    {
+        "text": "Rhode Island",
+        "value": "RI"
+    },
+    {
+        "text": "South Carolina",
+        "value": "SC"
+    },
+    {
+        "text": "South Dakota",
+        "value": "SD"
+    },
+    {
+        "text": "Tennessee",
+        "value": "TN"
+    },
+    {
+        "text": "Texas",
+        "value": "TX"
+    },
+    {
+        "text": "Utah",
+        "value": "UT"
+    },
+    {
+        "text": "Vermont",
+        "value": "VT"
+    },
+    {
+        "text": "Virgin Islands",
+        "value": "VI"
+    },
+    {
+        "text": "Virginia",
+        "value": "VA"
+    },
+    {
+        "text": "Washington",
+        "value": "WA"
+    },
+    {
+        "text": "West Virginia",
+        "value": "WV"
+    },
+    {
+        "text": "Wisconsin",
+        "value": "WI"
+    },
+    {
+        "text": "Wyoming",
+        "value": "WY"
+    }
+    ];
+
+    const departmentsContent = [
+        {
+            "text":"Sales",
+            "value":"Sales",
+        },
+        {
+            "text":"Marketing",
+            "value":"Marketing",
+        },
+        {
+            "text":"Human Resources",
+            "value":"HR",
+        },
+        {
+            "text":"Legal",
+            "value":"Legal",
+        }
+    ];
+
+ 
 
   return (
     <main>
@@ -73,26 +332,21 @@ function Home(props){
                 <input id="city" type="text" />
 
                 <label htmlFor="state">State</label>
-                <select name="state" id="state"></select>
+                <Select name="state" content={statesContent}></Select>
 
                 <label htmlFor="zip-code">Zip Code</label>
                 <input id="zipcode" type="number" />
             </fieldset>
 
             <label htmlFor="department">Department</label>
-            <select name="department" id="department">
-                <option>Sales</option>
-                <option>Marketing</option>
-                <option>Engineering</option>
-                <option>Human Resources</option>
-                <option>Legal</option>
-            </select>
+            <Select name="department" content={departmentsContent}></Select>
             
             <button type="submit">Save</button>
         </form>
 
         
       </div>
+
       { props.isEmployeeCreated ? 
         <div id="confirmation" className="modal">Employee Created!</div> : ""
       }
