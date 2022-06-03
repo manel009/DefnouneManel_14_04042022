3acc994fd2be8617d1e41e440a8e0124b9e2753b
Manel.Defnoune <36042435+manel009@users.noreply.github.com>
2022-06-03 21:31:41 +0200

    fixed console error

--- hr-net/src/components/select/Select.js
+++ hr-net/src/components/select/Select.js
@@ -4,7 +4,7 @@ export default function Select(props){
     return (
         <select id={props.name} name={props.name}>
             { props.content.map( contentline => {
-                    return <option value={contentline.value}> {contentline.text} </option>;
+                    return <option key={contentline.value} value={contentline.value}> {contentline.text} </option>;
                 }) 
             }
         </select>
