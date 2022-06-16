f4bb569ed5fb24545de7385ff2ffd2844afa71a2
Manel.Defnoune <36042435+manel009@users.noreply.github.com>
2022-06-16 14:05:14 +0200

    Datatable avec recherche + readme

--- hr-net/src/components/employee-list/EmployeeList.js
+++ hr-net/src/components/employee-list/EmployeeList.js
@@ -1,7 +1,7 @@
 import React  from 'react';
 import{connect} from "react-redux";
 import { Link } from "react-router-dom";
-import { DataGrid } from '@mui/x-data-grid';
+import MaterialTable from 'material-table';
 
 const mapStateToProps = state => {
     return {
@@ -12,40 +12,33 @@ const mapStateToProps = state => {
 
 function EmployeeList(props) {
 
-    const columns = [
-        { field: 'firstName', headerName: 'First name', width: 130 },
-        { field: 'lastName', headerName: 'Last name', width: 130 },
-        { field: 'startDate', headerName: 'Start Date', width: 110 },
-        { field: 'department', headerName: 'Department', width: 110 },
-        { field: 'dateOfBirth', headerName: 'Date of Birth', width: 110 },
-        { field: 'street', headerName: 'Street', width: 120 },
-        { field: 'city', headerName: 'City', width: 80 },
-        { field: 'state', headerName: 'State', width: 80 },
-        { field: 'zipCode', headerName: 'Zip Code', width: 90 },
+
+      const columns = [
+        { field: 'firstName', title: 'First name', width: 130 },
+        { field: 'lastName', title: 'Last name', width: 130 },
+        { field: 'startDate', title: 'Start Date', width: 110 },
+        { field: 'department', title: 'Department', width: 110 },
+        { field: 'dateOfBirth', title: 'Date of Birth', width: 110 },
+        { field: 'street', title: 'Street', width: 120 },
+        { field: 'city', title: 'City', width: 80 },
+        { field: 'state', title: 'State', width: 80 },
+        { field: 'zipCode', title: 'Zip Code', width: 90 },
       ];
-
-    const rows = props.employeeList;
-
-    var id=0;
-    const getRowID = () => {
-        return id++;
-    }
-
-    console.log(rows);
-
 
     return (
       <div id="employee-div" className="container">
             <h1>Current Employees</h1>
             <div className='employee-list'>
-                <DataGrid
-                    rows={rows}
-                    columns={columns}
-                    pageSize={5}
-                    rowsPerPageOptions={[5]}
-                    checkboxSelection
-                    getRowId={getRowID}
-                    scrollbarSize={1}
+            <MaterialTable
+                title="Current employees"
+                columns={columns}
+                data={props.employeeList}       
+                options={{
+                    sorting: true,
+                    search: true,
+                    filtering: true,
+                    selection: true
+                }}
                 />
             </div>
             <Link to="/"><button>Home</button></Link>
